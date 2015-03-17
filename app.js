5daf7f48a1d919733f73b8d7474890927c850fc4
tako-black <gennarishock+tako@gmail.com>
2015-03-17 14:59:33 +0900

    step03

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-angular.module('App', []);
+angular.module('App', ['ngRoute']).controller('MainController', ['$scope', function ($scope) {
+ $scope.todos = [];
+
+ $scope.addTodo = function () {
+  $scope.todos.push({
+   title: Math.random(),
+   done: false
+  });
+ };
+}]);
