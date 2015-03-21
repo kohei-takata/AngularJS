3e1ed9d529df8f2f3b5d91d9b454c0fdf1a9af46
tako-black <gennarishock+tako@gmail.com>
2015-03-21 11:43:42 +0900

    step03

--- chohyo/app.js
+++ chohyo/app.js
@@ -1,1 +1,16 @@
-angular.module('App', ['ngRoute']);
+angular.module('App', ['ngRoute'])
+ .config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+   .when('/', {
+    templateUrl: 'index-tmpl'
+   })
+   .when('/new', {
+    templateUrl: 'new-tmpl'
+   })
+   .when('/sheet/:id', {
+    templateUrl: 'sheet-tmpl'
+   })
+   .otherwise({
+    redirectTo: '/'
+   });
+ }]);
